d5df0882286506ee331aebdfcd10f8d759168bcc
Ayesha <ayesha.omarali@gmail.com>
2018-10-30 15:30:54 -0700

    remove unnecessary styling

--- tests/js/spec/views/organizationDiscover/discover.spec.jsx
+++ tests/js/spec/views/organizationDiscover/discover.spec.jsx
@@ -96,11 +96,7 @@ describe('Discover', function() {
       firstPageMock = MockApiClient.addMockResponse({
         url: '/organizations/org-slug/discover/query/?per_page=1000&cursor=0:0:1',
         method: 'POST',
-        body: {
-          timing: {},
-          data: [],
-          meta: [],
-        },
+        body: {timing: {}, data: [], meta: []},
         headers: {
           Link:
             '<api/0/organizations/sentry/discover/query/?per_page=2&cursor=0:0:1>; rel="previous"; results="false"; cursor="0:0:1", <api/0/organizations/sentry/discover/query/?per_page=1000&cursor=0:2:0>; rel="next"; results="true"; cursor="0:1000:0"',
